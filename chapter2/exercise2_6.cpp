bc05ac429016f87e5ce9601af677f447dd163589
Dilshodbek Ikromov <141973902+DilshodbekIkromov@users.noreply.github.com>
2025-06-19 02:17:31 +0500

    some changes

--- chapter2/exercise2_6.cpp
+++ chapter2/exercise2_6.cpp
@@ -4,16 +4,18 @@
 using namespace std;
 
 long double f(long double x){ 
-    return expl(x) + x*x*x - 5.0L; 
+    return expl(x) + x * x * x - 5.0L; 
 }
 long double fprime(long double x){ 
-    return expl(x) + 3.0L*x*x; 
+    return expl(x) + 3.0L * x * x; 
 }
 
-int main() {    
+int main() {
     long double eps;
+    cout << "Enter desired precision (eps): ";
     cin >> eps;
-    if (cin.fail() || eps <= 0.0L){
+    if (cin.fail() || eps <= 0.0L || eps >= 1.0L) {
+        cout << "Invalid input. Exiting.\n";
         return 0;
     }
 
@@ -22,14 +24,15 @@ int main() {
 
     for (int iter = 1; ; ++iter) {
         long double denom = fprime(x_prev);
-        if (fabsl(denom) < 1e-18L) {          
-            cout << iter-1 << '\n' << fixed << setprecision(10) << x_prev << '\n';
+        if (fabsl(denom) < 1e-18L) {
+            cout << "Stopped: derivative too small after " << iter - 1 << " iterations.\n";
+            cout << "Last approximation: " << fixed << setprecision(10) << x_prev << '\n';
             return 0;
         }
         long double x_next = x_prev - f(x_prev) / denom;
-
         if (fabsl(x_next - x_prev) < eps || iter == MAX_ITERS) {
-            cout << iter << '\n' << fixed << setprecision(10) << x_next << '\n';
+            cout << "Converged after " << iter << " iterations.\n";
+            cout << "Root approximation: " << fixed << setprecision(10) << x_next << '\n';
             return 0;
         }
         x_prev = x_next;
