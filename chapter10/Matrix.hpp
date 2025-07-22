e918de36a8b4844959d202b05fa14f8db5074874
wizard <141973902+DilshodbekIkromov@users.noreply.github.com>
2025-07-22 22:38:37 +0500

    Simplify chapter10 comments

--- chapter10/Matrix.hpp
+++ chapter10/Matrix.hpp
@@ -2,38 +2,37 @@
 #define MATRIX_HPP
 #include "Vector.hpp"
 
-// A mathematical matrix class for storing and manipulating 2D arrays of numbers
+// Basic matrix class
 class Matrix
 {
 private:
-    double** mData;    // Pointer to 2D array storing matrix elements (rows of pointers to columns)
-    int mNumRows;      // Number of rows in the matrix (height)
-    int mNumCols;      // Number of columns in the matrix (width)
+    double** mData;  // matrix entries
+    int mNumRows;    // rows
+    int mNumCols;    // columns
 public:
-    Matrix(const Matrix& otherMatrix);     // Copy constructor: create matrix by copying another matrix
-    Matrix(int numRows, int numCols);      // Constructor: create empty matrix with specified rows and columns
-    ~Matrix();                             // Destructor: free memory allocated for the matrix
+    Matrix(const Matrix& otherMatrix);
+    Matrix(int numRows, int numCols);
+    ~Matrix();
 
-    int GetNumberOfRows() const;           // Get the number of rows (height) in the matrix
-    int GetNumberOfColumns() const;        // Get the number of columns (width) in the matrix
+    int GetNumberOfRows() const;
+    int GetNumberOfColumns() const;
 
-    double& operator()(int i, int j);      // Access element at row i, column j (can modify the value)
-    Matrix& operator=(const Matrix& otherMatrix); // Assignment: copy all elements from another matrix
-    Matrix operator+() const;             // Unary plus: return a positive copy of the matrix
-    Matrix operator-() const;             // Unary minus: return matrix with all elements negated
-    Matrix operator+(const Matrix& m1) const; // Addition: add corresponding elements of two matrices
-    Matrix operator-(const Matrix& m1) const; // Subtraction: subtract corresponding elements of two matrices
+    double& operator()(int i, int j);
+    Matrix& operator=(const Matrix& otherMatrix);
+    Matrix operator+() const;
+    Matrix operator-() const;
+    Matrix operator+(const Matrix& m1) const;
+    Matrix operator-(const Matrix& m1) const;
 
-    Matrix operator*(double a) const;     // Scalar multiplication: multiply each element by a number
-    double CalculateDeterminant() const;  // Calculate determinant (only works for square matrices)
+    Matrix operator*(double a) const;
+    double CalculateDeterminant() const;
 
-    // Friend functions for matrix-vector multiplication (can access private members)
-    friend Vector operator*(const Matrix& m, const Vector& v);  // Matrix times vector: m * v
-    friend Vector operator*(const Vector& v, const Matrix& m);  // Vector times matrix: v * m
+    friend Vector operator*(const Matrix& m, const Vector& v);
+    friend Vector operator*(const Vector& v, const Matrix& m);
 };
 
-// Stand-alone function declarations for matrix-vector operations
-Vector operator*(const Matrix& m, const Vector& v);  // Multiply matrix by vector (m * v)
-Vector operator*(const Vector& v, const Matrix& m);  // Multiply vector by matrix (v * m)
+// Multiply matrix and vector
+Vector operator*(const Matrix& m, const Vector& v);
+Vector operator*(const Vector& v, const Matrix& m);
 
 #endif
--- chapter10/Vector.hpp
+++ chapter10/Vector.hpp
@@ -1,38 +1,34 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
-// A mathematical vector class for storing and manipulating arrays of numbers
-class Vector 
+// Simple vector class
+class Vector
 {
-    private:
-        double* mData; // pointer to dynamically allocated array storing vector elements
-        int mSize;     // number of elements in the vector
-        
-    public: 
-        Vector(const Vector& otherVector); // create vector by copying another vector
-        Vector(int size);                  // create vector with specified size
-        ~Vector();                         // destructor to free memory
+private:
+    double* mData; // data array
+    int mSize;     // length
+public:
+    Vector(const Vector& otherVector);
+    Vector(int size);
+    ~Vector();
 
-        int GetSize() const;               // returns the size of the vector
-        double& operator[] (int i);        // access element at index i (can modify)
+    int GetSize() const;
+    double& operator[](int i);
+    double Read(int i) const;
+    double& operator()(int i);
 
-        double Read(int i) const;          // read element at index i (read-only)
-        double& operator() (int i);        // alternative way to access element at index i
+    Vector& operator=(const Vector& otherVector);
+    Vector operator+() const;
+    Vector operator-() const;
+    Vector operator+(const Vector& v1) const;
+    Vector operator-(const Vector& v1) const;
+    Vector operator*(double a) const;
+    double CalculateNorm(int p=2) const;
 
-        Vector& operator=(const Vector& otherVector); // assign one vector to another
-        Vector operator+() const;          // unary plus (returns copy of vector)
-        Vector operator-() const;          // unary minus (negates all elements)
-        Vector operator+(const Vector& v1) const; // add two vectors element-wise
-        Vector operator-(const Vector& v1) const; // subtract two vectors element-wise
-
-        Vector operator*(double a) const;  // multiply vector by a scalar
-        double CalculateNorm(int p=2) const; // calculate p-norm of vector (default: 2-norm)
-
-        friend int length(const Vector& v); // friend function to get vector length
+    friend int length(const Vector& v);
 };
 
-// Friend function declaration (note the semicolon)
+// Friend function to get size
 int length(const Vector& v);
 
 #endif
-
